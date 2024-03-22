a7ffec1fd8b3826f5e34fd1a83c7f680935bc6c9
abdullal <abdullahnazir.099@gmail.com>
2024-03-22 12:53:21 +0500

    fourth

--- dbtest.js
+++ dbtest.js
@@ -8,15 +8,33 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching users:', error));
 
-    fetch('/.netlify/functions/getProducts')
-        .then(response => response.json())
-        .then(products => {
-            const productsContainer = document.getElementById('products');
-            const productList = products.map(product => `<li>${product.product_name} - ${product.product_price}, Category: ${product.category}</li>`).join('');
-            productsContainer.innerHTML = `<ul>${productList}</ul>`;
-            // Display products data similarly to users
-        })
-        .catch(error => console.error('Error fetching products:', error));
+    // fetch('/.netlify/functions/getProducts')
+    //     .then(response => response.json())
+    //     .then(products => {
+    //         const productsContainer = document.getElementById('products');
+    //         const productList = products.map(product => `<li>${product.product_name} - ${product.product_price}, Category: ${product.category}</li>`).join('');
+    //         productsContainer.innerHTML = `<ul>${productList}</ul>`;
+           
+    //     })
+    //     .catch(error => console.error('Error fetching products:', error));
+
+
+    
+            fetch('/.netlify/functions/getProducts')
+              .then(response => response.json())
+              .then(products => {
+                const productsContainer = document.getElementById('products');
+                const productList = products.map(product => `
+                  <li class="product-item">
+                    <span class="product-name">${product.product_name}</span>
+                    <span class="product-price">$${product.product_price}</span>
+                    <span class="product-category">Category: ${product.category}</span>
+                  </li>
+                `).join('');
+                productsContainer.innerHTML = `<ul class="product-list">${productList}</ul>`;
+              })
+              .catch(error => console.error('Error fetching products:', error));
+       
 
     fetch('/.netlify/functions/getOrders')
         .then(response => response.json())
