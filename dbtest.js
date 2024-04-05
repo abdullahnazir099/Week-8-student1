ea35540fe36a53a61f386a21eea3e97e99e4ea26
abdullal <abdullahnazir.099@gmail.com>
2024-04-05 12:12:54 +0500

    first

--- dbtest.js
+++ dbtest.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('/.netlify/functions/getUsers')
-        .then(response => response.json())
-        .then(users => {
-            const usersContainer = document.getElementById('users');
-            const userList = users.map(user =>
-               ` 
+  fetch('/.netlify/functions/getUsers')
+    .then(response => response.json())
+    .then(users => {
+      const usersContainer = document.getElementById('users');
+      const userList = users.map(user =>
+        ` 
                <li>Name :${user.username} - Email :${user.email}, 
                Signed up on: ${user.signup_date}</li>`)
-            .join('');
-            usersContainer.innerHTML = `<ul>${userList}</ul>`;
-        })
-        .catch(error => console.error('Error fetching users:', error));
+        .join('');
+      usersContainer.innerHTML = `<ul>${userList}</ul>`;
+    })
+    .catch(error => console.error('Error fetching users:', error));
 
 
 
 
 
-    fetch('/.netlify/functions/getProducts')
-        .then(response => response.json())
-        .then(products => {
-            const productsContainer = document.getElementById('products');
-            const productList = products.map(product => `
+  fetch('/.netlify/functions/getProducts')
+    .then(response => response.json())
+    .then(products => {
+      const productsContainer = document.getElementById('products');
+      const productList = products.map(product => `
           
              
                   <li class="product-item">
@@ -29,16 +29,16 @@ document.addEventListener('DOMContentLoaded', function () {
                     <span class="product-category">Category: ${product.category}</span>
                   </li>
                 `).join('');
-            productsContainer.innerHTML = `<ul class="product-list">${productList}</ul>`;
-        })
-        .catch(error => console.error('Error fetching products:', error));
+      productsContainer.innerHTML = `<ul class="product-list">${productList}</ul>`;
+    })
+    .catch(error => console.error('Error fetching products:', error));
 
 
-    fetch('/.netlify/functions/getOrders')
-        .then(response => response.json())
-        .then(orders => {
-            const ordersContainer = document.getElementById('orders');
-            const orderList = orders.map(order=> `
+  fetch('/.netlify/functions/getOrders')
+    .then(response => response.json())
+    .then(orders => {
+      const ordersContainer = document.getElementById('orders');
+      const orderList = orders.map(order => `
           
             <li class="order-item">
               <span class="order-id">ID:${order.order_id}</span>
@@ -49,10 +49,10 @@ document.addEventListener('DOMContentLoaded', function () {
             </li>
           `).join('');
       ordersContainer.innerHTML = `<ul class="order-list">${orderList}</ul>`;
-           
-        })
-        .catch(error => console.error('Error fetching orders:', error));
+
+    })
+    .catch(error => console.error('Error fetching orders:', error));
 
 
-        
+
 });
